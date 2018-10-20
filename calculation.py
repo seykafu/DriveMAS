344bb6f0d0a29d8ef0f7cae0148957973c2318e8
vanstorm9 <antlowhur@yahoo.com>
2018-10-20 10:36:17 -0400

    Added cost function

--- calculation.py
+++ calculation.py
@@ -2,14 +2,24 @@
 def calcCurrCoins(carbonEmiss, milage):
 	# Calculate amount of coins based from stats
 
-	return (10**6)/(carbonEmiss*milage)
+	return int((10**6)/(carbonEmiss*milage))
 
 def calcPassiveRefCoins(carbonEmiss, milage):
 	# Calculate amount of coins from person you referred to
-	return 0.25*((10**6)/(carbonEmiss*milage))
+	return int(0.25*((10**6)/(carbonEmiss*milage)))
 
 def calcReferalCoins():
 	# Amount of coins you would get from referring someone
 	
-	# Temperarly, it's an abritrary vaue
+	# Temporarily, it's an arbitrary value
 	return 5
+
+def getCostFromGas(stateGasCost, milesPerGallon):
+	
+	return stateGasCost*milesPerGallon
+
+
+def getCostfromElectric(stateElecCost, milesPerBatt):
+
+	return stateElecCost*milesPerBatt
+
