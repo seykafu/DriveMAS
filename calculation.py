ac0420bd199c93f0f9f64c543ff55e9c39d9d270
vanstorm9 <antlowhur@yahoo.com>
2018-10-20 10:29:50 -0400

    Editing formula

--- calculation.py
+++ calculation.py
@@ -2,11 +2,11 @@
 def calcCurrCoins(carbonEmiss, milage):
 	# Calculate amount of coins based from stats
 
-	return 1000/(carbonEmiss*milage)
+	return (10**6)/(carbonEmiss*milage)
 
 def calcPassiveRefCoins(carbonEmiss, milage):
 	# Calculate amount of coins from person you referred to
-	return 0.25*(1000/(carbonEmiss*milage))
+	return 0.25*((10**6)/(carbonEmiss*milage))
 
 def calcReferalCoins():
 	# Amount of coins you would get from referring someone
