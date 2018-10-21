780354daf98cefb16629425af4437f1b24ba8adc
vanstorm9 <antlowhur@yahoo.com>
2018-10-21 03:16:47 -0400

    Adding some stuff

--- accountFunc.py
+++ accountFunc.py
@@ -6,12 +6,12 @@ sqlpy.selectCmd("SELECT * FROM account WHERE password='hackers'")
 
 '''
 
-def createAccount(username, password, carbon):
-	sqlCom = 'INSERT INTO account (username, password, coins, currCO2, currMilage) values (%s,%s,%s,%s,%s)'
+def createAccount(username, password, carType):
+	sqlCom = 'INSERT INTO account (username, password, coins, carType, currMilage) values (%s,%s,%s,%s,%s)'
 
 	cursor = sqlpy.connection.cursor()
 	try:
-		cursor.execute(sqlCom, (username,password,0.0,0.0,0.0))
+		cursor.execute(sqlCom, (username,password,0.0,carType,0.0))
 	except:
 		print 'There was a problem creating an account in database'
 		return -1
@@ -39,9 +39,8 @@ def updateMilageById(idNum, milage):
 	sqlpy.sequel("UPDATE account SET currMilage="+milage+" WHERE id="+idNum+";")
 
 
-def updateCO2ById(idNum, co2):
-	# Theoreticly, you should not have to use this function unless you are switching from GV to EV and vice versa
-	sqlpy.sequel("UPDATE account SET currCO2="+co2+" WHERE id="+idNum+";")
+def updateCarTypeById(idNum, carType):
+	sqlpy.sequel("UPDATE account SET carType="+carType+" WHERE id="+idNum+";")
 
 def updateCoinsById(idNum, coinValue):
 	sqlpy.sequel("UPDATE account SET coins="+coinValue+" WHERE id="+idNum+";")
